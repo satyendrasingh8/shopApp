a3df1613cbb6076bdf00e69049f6437b4485c1e4
satyendrasingh8 <satyendrasinghsss7398@gmail.com>
2020-11-08 08:17:48 +0530

    finishing cart screen

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shopApp/models/providers/cart.dart';
+import 'package:shopApp/widgets/cart_item.dart' as ci;
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -48,12 +49,14 @@ class CartScreen extends StatelessWidget {
           Expanded(
             child: ListView.builder(
               itemCount: cart.items.length,
-              itemBuilder: (ctx,index) => Card(),
+              itemBuilder: (ctx,i) => ci.CartItem(
+                cart.items.values.toList()[i].id,cart.items.values.toList()[i].price,
+              cart.items.values.toList()[i].quantity,cart.items.values.toList()[i].title),
 
 
               ),
           
-          ,)
+          )
         ],
       ),
     );
