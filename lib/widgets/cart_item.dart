8c7702acaebc200be10c76e004ff2a9ea06816a4
satyendrasingh8 <satyendrasinghsss7398@gmail.com>
2020-11-08 09:04:25 +0530

    delete cart items using dismissible widget

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -10,17 +10,21 @@ class CartItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: FittedBox(child: Text('\$$price')),
+    return Dismissible(
+       key: ValueKey(id),
+       background: Container(color: Theme.of(context).accentColor,),
+          child: Card(
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: FittedBox(child: Text('\$$price')),
+            ),
+            title: Text(title),
+            subtitle: Text('Total: \$${(price * quantity)}'),
+            trailing: Text('$quantity x'),
           ),
-          title: Text(title),
-          subtitle: Text('Total: \$${(price * quantity)}'),
-          trailing: Text('$quantity x'),
         ),
       ),
     );
