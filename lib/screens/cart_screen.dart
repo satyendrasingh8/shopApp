0df574b497f362d5b1511f7a6f6007979af50dec
satyendrasingh8 <satyendrasinghsss7398@gmail.com>
2020-11-09 10:28:31 +0530

    finshing order screen

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -28,7 +28,7 @@ class CartScreen extends StatelessWidget {
                   ),
                   Spacer(),
                   Chip(
-                    label: Text('\$${cart.totalAmount}'),
+                    label: Text('\$${cart.totalAmount.toStringAsFixed(2)}'),
                     backgroundColor: Theme.of(context).primaryColor,
                   ),
                   FlatButton(
