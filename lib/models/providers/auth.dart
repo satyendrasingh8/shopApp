b49b36dd4e8438ac6e5d92db33ca2f3e27047d33
satyendrasingh8 <satyendrasinghsss7398@gmail.com>
2020-11-19 10:50:11 +0530

    auto logOut  functionality using dart Async package

--- lib/models/providers/auth.dart
+++ lib/models/providers/auth.dart
@@ -10,7 +10,7 @@ class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
-
+  Timer _authTimer;
   bool get isAuth {
     return token != null;
   }
@@ -70,6 +70,7 @@ class Auth with ChangeNotifier {
       _userId = responseData['localId'];
       _expiryDate = DateTime.now()
           .add(Duration(seconds: int.parse(responseData['expiresIn'])));
+      _autoLogout();
       notifyListeners();
     } catch (error) {
       throw error;
@@ -80,11 +81,18 @@ class Auth with ChangeNotifier {
     _token = null;
     _userId = null;
     _expiryDate = null;
+    if (_authTimer != null) {
+      _authTimer.cancel();
+      _authTimer = null;
+    }
     notifyListeners();
   }
 
-
-
-
-
+  void _autoLogout() {
+    if (_authTimer != null) {
+      _authTimer.cancel();
+    }
+    final timeToExpiry = _expiryDate.difference(DateTime.now()).inSeconds;
+    _authTimer = Timer(Duration(seconds: timeToExpiry), logout);
+  }
 }
